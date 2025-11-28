4c9ffd085326bef00f1f75f8531f010179bd34d5
Andre Augusto <andre.augusto222@gmail.com>
2025-11-28 14:46:55 -0300

    configure forwarded headers in Program.cs

--- src/UsersApi/Program.cs
+++ src/UsersApi/Program.cs
@@ -8,6 +8,19 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | 
+                       Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+});
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All,
+    KnownNetworks = { }, 
+    KnownProxies = { }
+});
+
 app.MapOpenApi();
 
 app.UseAuthorization();
