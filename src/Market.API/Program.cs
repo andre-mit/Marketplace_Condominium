4dc2541b19a7360c4c63f167bfafc91ee09eb2c2
Andre Augusto <andre.augusto222@gmail.com>
2025-11-28 11:14:34 -0300

    remove comments

--- src/Market.API/Program.cs
+++ src/Market.API/Program.cs
@@ -105,12 +105,8 @@ if (shouldSeed)
     await dbContext.Database.EnsureCreatedAsync();
 }
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 
